ea5c1c2abe4d0e6fa0cfa809e558c3a26ac1ed97
codekappal <captain@kappal.in>
2024-08-07 18:18:05 +0530

    updated exports

--- app/index.js
+++ app/index.js
@@ -1,9 +1,2 @@
-export { default as KappalAccordion } from './components/KappalAccordion';
-export { default as KappalAutoComplete } from './components/KappalAutoComplete';
-export { default as KappalAvatar } from './components/KappalAvatar';
-export { default as KappalBadge } from './components/KappalBadge';
-export { default as KappalBreadcrumbs } from './components/KappalBreadcrumbs';
-export { default as KappalButton } from './components/KappalButton';
-export { default as KappalCard } from './components/KappalCard';
-export { default as KappalInput } from './components/KappalInput';
-export { default as KappalSelect } from './components/KappalSelect';
+export * from '@nextui-org/react';
+export * from './src/components';
